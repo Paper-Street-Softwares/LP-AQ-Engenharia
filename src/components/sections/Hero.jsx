08752b12870a419131c5d2704fdc521769ad66bc
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-12-15 11:06:52 -0300

    fix: Titulo da hero ajustado no pt

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -60,7 +60,7 @@ export default function Hero({
 
         <MotionDivDownToUp>
           <div
-            className={`flex justify-center desktop1:justify-start font-medium leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7 mb-2 ${titleTextColor}`}
+            className={`flex justify-center desktop1:justify-start leading-[40px] phone3:leading-[42px] tablet1:leading-[70px] desktop1:leading-[60px] desktop2:leading-[65px] text-center desktop1:text-left text-title4 phone2:text-title5 phone3:text-title5 tablet1:text-title7 mb-2 ${titleTextColor}`}
           >
             <h1
               className=""
